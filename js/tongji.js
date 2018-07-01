fbc1c57dbe36cb32ed87c83c7c903fb429c59ca2
yysue <ch001ao@qq.com>
2018-07-01 23:40:47 +0800

    add v2

--- js/tongji.js
+++
@@ -1,9 +0,0 @@
-/* baidu tongji */
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?f3d058bd2a023d2a6b01922a17c1bb20";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-
